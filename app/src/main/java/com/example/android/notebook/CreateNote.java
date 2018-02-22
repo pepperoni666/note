cba1cf5c0a303b5ab55499187d502465977ba7ef
pepperoni666 <peretser08@gmail.com>
2018-02-22 11:01:41 +0100

    Variables done in CreateNote.class

--- app/src/main/java/com/example/android/notebook/CreateNote.java
+++ app/src/main/java/com/example/android/notebook/CreateNote.java
@@ -2,15 +2,33 @@ package com.example.android.notebook;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by peret on 22.02.2018.
  */
 
 public class CreateNote extends AppCompatActivity {
+    EditText note_title;
+    EditText note_text;
+    Button save_button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.edit_note);
+
+        note_title = findViewById(R.id.title);
+        note_text = findViewById(R.id.textOfTheNote);
+        save_button = findViewById(R.id.sabeButton);
+
+        save_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: save to satabase |
+            }
+        });
     }
 }
